7542428bbc64b5f37c327bc6f0b934dc3b62dde8
fhh <2297763550@qq.com>
2018-12-27 15:16:19 +0800

    首次提交

---
+++ supply_chain_api/src/main/java/com/fhhService/CommodityService.java
@@ -0,0 +1,11 @@
+package com.fhhService;
+
+import java.util.List;
+
+import com.pojo.Commodity;
+
+public interface CommodityService {
+
+	
+	List<Commodity> listCommodity();
+}
---
+++ supply_chain_api/src/main/java/com/pojo/Commodity.java
@@ -0,0 +1,24 @@
+package com.pojo;
+
+import java.io.Serializable;
+
+public class Commodity implements Serializable{
+
+	
+	private int c_id;
+	private String commodity_name;
+	public int getC_id() {
+		return c_id;
+	}
+	public void setC_id(int c_id) {
+		this.c_id = c_id;
+	}
+	public String getCommodity_name() {
+		return commodity_name;
+	}
+	public void setCommodity_name(String commodity_name) {
+		this.commodity_name = commodity_name;
+	}
+	
+	
+}
